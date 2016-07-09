afdd69ab3544627bde044ae0c0e6558cbe18ec58
Oshan Rube <oshanrube@gmail.com>
2016-07-09 09:12:55 +0530

    finish setting up the login page

--- src/Onic/UserBundle/Entity/User.php
+++ src/Onic/UserBundle/Entity/User.php
@@ -2,20 +2,28 @@
 
 namespace Onic\UserBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
 /**
  * User
  */
-class User
+class User extends BaseUser
 {
     /**
      * @var integer
      */
-    private $id;
+    protected $id;
 
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 
     /**
      * Get id
-     *
      * @return integer
      */
     public function getId()
